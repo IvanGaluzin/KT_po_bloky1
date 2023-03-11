83d1bfae32d275995c803a646dcfe5c2e709e225
Ivan Galuzin <galuzin627@gmail.com>
2023-03-11 11:46:52 +0700

    функция отбора по условию из масива

--- KT/Program.cs
+++ KT/Program.cs
@@ -1,1 +1,14 @@
 ﻿
+
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
